3b85329d57c0ba67289b8da9b75785ba22cd58fe
Tom Kelly <tom@getcatalyst.io>
2020-07-12 09:03:23 -0400

    add pointer controls at the ready

--- public/three.mjs
+++ public/three.mjs
@@ -1,5 +1,9 @@
 import * as THREE from 'https://unpkg.com/three@0.118.3/build/three.module.js'
 import {OrbitControls} from 'https://unpkg.com/three@0.118.3/examples/jsm/controls/OrbitControls.js'
+// import {PointerLockControls} from 'https://unpkg.com/three@0.118.3/examples/jsm/controls/PointerLockControls.js'
 
 export default THREE
-export { OrbitControls }
+export {
+  OrbitControls,
+  // PointerLockControls,
+}
